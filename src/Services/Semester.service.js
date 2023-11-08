b2a44f2349a4f8a99e42c9747fc9367acfac6543
Leadstoyou <lytieulong2j2@gmail.com>
2023-11-08 13:02:43 +0700

    lmeo

--- src/Services/Semester.service.js
+++ src/Services/Semester.service.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://localhost:8080/semesters";
+const API_BASE = "http://localhost:8080/systemSetting/semester";
 const getAllSemester = async () => {
   try {
     const result = await axios.get(API_BASE);
